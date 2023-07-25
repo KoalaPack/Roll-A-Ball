ae41a71eb0a68465a8f8770a07c8c4fa5e234342
Patrick <cptty360@gmail.com>
2023-07-25 11:42:25 +1000

    Basic Main Menu

---
+++ Assets/Scripts/Menu/SceneController.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    //Different menues
+    public GameObject mainMenu;
+    public GameObject levelSelect;
+    public GameObject options;
+
+    //Setting all scenes to their start position
+    public void Start()
+    {
+        mainMenu.SetActive(true);
+        levelSelect.SetActive(false);
+        options.SetActive(false);
+    }
+
+    //Deactivates the menu UI layer and activates the options layer
+    public void OptionButton()
+    {
+        mainMenu.SetActive(false);
+        options.SetActive(true);
+    }
+    //Deactivates the menu UI layer and activates the level select layer
+    public void StartButton()
+    {
+        mainMenu.SetActive(false);
+        levelSelect.SetActive(true);
+    }
+
+    public void backToMenu()
+    {
+        mainMenu.SetActive(true);
+        levelSelect.SetActive(false);
+        options.SetActive(false);
+    }
+
+    //Will change our scene to the string passed in 
+    public void ChangeScene(string _sceneName)
+    {
+        SceneManager.LoadScene(_sceneName);
+    }
+
+    //Reloads the current scene we are in
+    public void RelodeScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    //Loads Main Menu Scene
+    public void ToTitleGame()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    //Loads Normal Game Scene
+    public void ToNormalGame()
+    {
+        SceneManager.LoadScene("NormalGame");
+    }
+
+    //Loads Tron Game Scene
+    public void ToTronGame()
+    {
+        SceneManager.LoadScene("TronGame");
+    }
+
+    //Gets the active scene name
+    public string GetSceneName()
+    {
+        return SceneManager.GetActiveScene().name;
+    }
+
+    //Quit Button
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
