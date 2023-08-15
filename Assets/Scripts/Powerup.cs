e0be569783ad3434158b2578d9e5adce90c7c6e7
Patrick <cptty360@gmail.com>
2023-08-16 01:33:51 +1000

    All Powerups functioning

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Powerup : MonoBehaviour
 {
     //An enum is a datatype that we can specify its values an and use
-    public enum PowerupType {SpeedUp, SpeedDown, SizeUp, SizeDown}
+    public enum PowerupType {SpeedUp, SpeedDown, Grow, Shrink}
 
     public PowerupType myPowerup;       //This objects power up type
     public float powerupDuration = 2f;  //The duration of the powerup
@@ -15,6 +15,49 @@ public class Powerup : MonoBehaviour
     private void Start()
     {
         //Find and assign the player controller object to this local reference
-        
+        playerController = FindObjectOfType<PlayerController>();       
+
+    }
+
+    public void UsePowerup()
+    {
+        //If this powerup is the speedup powerup, increase the player controller speed by double
+        if (myPowerup == PowerupType.SpeedUp)
+            playerController.speed = playerController.baseSpeed * 2;
+
+        if (myPowerup == PowerupType.SpeedDown)
+            playerController.speed = playerController.baseSpeed / 3;
+
+        //If this powerup is the grow powerup, increase the player controller size by double
+        //We also need to move it up on the y axis otherwise it will go through the ground collider
+        if (myPowerup == PowerupType.Grow)
+        {
+            Vector3 temp = playerController.gameObject.transform.position;
+            temp.y = 1;
+            playerController.gameObject.transform.position = temp;
+            playerController.gameObject.transform.localScale = Vector3.one * 2;
+        }
+
+        //If this powerup is the shrink powerup, decrease the player size by half
+        if (myPowerup == PowerupType.Shrink)
+            playerController.gameObject.transform.localScale = Vector3.one / 2;
+
+        //Start a coroutine to reset the powerups effects
+        StartCoroutine(ResetPowerup());
+    }
+
+    IEnumerator ResetPowerup()
+    {
+        yield return new WaitForSeconds(powerupDuration);
+
+        //If this powerup relates to speed, reset our player controller speed to its base speed
+        if(myPowerup == PowerupType.SpeedUp || myPowerup == PowerupType.SpeedDown)
+            playerController.speed = playerController.baseSpeed;
+
+        //If this powerup relates to size, reset our player controller size to 1
+        if (myPowerup == PowerupType.Grow || myPowerup == PowerupType.Shrink)
+        {
+            playerController.gameObject.transform.localScale = Vector3.one;
+        }
     }
 }
--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -4,10 +4,35 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
+    public enum PowerupType { SpeedUp, SizeUp }
+
+    public PowerupType myPowerup;       // This object's power up type
+
     public float speed; // The rotation speed of the object
+
+    private bool isScalingUp = false;
+    private Vector3 originalScale;
+    public float scaleSpeed = 1.0f; // The speed at which the object scales
+
+    private void Start()
+    {
+        originalScale = transform.localScale;
+    }
+
     void Update()
     {
-        // Rotate the object based on the axis (Vector3) and speed 
-        transform.Rotate(new Vector3(3, 30, 0) * Time.deltaTime * speed);
+        if (myPowerup == PowerupType.SpeedUp)
+        {
+            // Rotate the object based on the axis (Vector3) and speed 
+            transform.Rotate(new Vector3(4, 30, 4) * Time.deltaTime * speed);
+        }
+
+        if (myPowerup == PowerupType.SizeUp)
+        {
+            // Gradually scale up and down the object
+            float scaleAmount = Mathf.PingPong(Time.time * scaleSpeed, .5f); // PingPong between 0 and 1
+            Vector3 newScale = originalScale + originalScale * scaleAmount;
+            transform.localScale = newScale;
+        }
     }
 }
